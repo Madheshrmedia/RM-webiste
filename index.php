65a36ce64dfa3b1994c349eaee0caaf6f5a3281b
Madheshrmedia <madhesh.rmedia1123@gmail.com>
2024-11-27 16:15:21 +0530

    smm frame completed

--- index.php
+++ index.php
@@ -1,3 +1,61 @@
 <?php
 include_once "./header.php";
 ?>
+
+<style>
+    .smm-head {
+        color: #D32F2F;
+    }
+
+    .smm-head {
+        font-size: 40px;
+        font-weight: 700;
+    }
+
+    .smm-cont {
+        color: #425466;
+        font-size: 17px;
+    }
+
+    .smm-link {
+        color: white;
+        text-decoration: none;
+        background: #B71C1C;
+        padding: 13px 20px;
+    }
+
+    .top_ser_frame {
+        background: #FCF6F6;
+    }
+</style>
+
+<body>
+    <img src="./image/banner.webp" class="img-fluid" alt="">
+
+    <div class="container-fluid top_ser_frame my-5">
+        <div class="row ">
+            <div class="col-md-10 mx-auto">
+                <div class="row mt-5">
+                    <div class="col-md-6 my-auto">
+                        <div class="smm-head">
+                            Social Media <br> Marketing
+                        </div>
+                        <p class="smm-cont mt-4 mb-5">
+                            Our team crafts tailored social media strategies to <br> increase your brand's visibility.
+                            From
+                            organic growth to <br>paid ads, we help you connect with your audience <br> effectively
+                        </p>
+                        <a href="#" class="smm-link">
+                            Grow Your Audience
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="./image/sm-img.webp" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
