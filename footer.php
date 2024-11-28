70709e1cc0ee56a8407986a69dad8294506af2e3
Muthubala <muthubala894@gmail.com>
2024-11-28 16:17:26 +0530

    Footer

---
+++ footer.php
@@ -0,0 +1,221 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+        <style>
+             .footerbanner{
+                background-color: #C92828;
+                padding:3rem 0 0 0;
+ 
+   color:#FFFFFF;
+            }
+            .footban{
+                padding-top: 16rem;
+                text-align: center;
+                background-color: #C92828;
+                background-image: url(./image/footbanner.png);
+   background-repeat: no-repeat;
+   background-size: cover;
+   color:#FFFFFF;
+   position: relative;
+      width: 100%;
+            }
+            .footban img {
+      width: 100%;
+      height: auto;
+      display: block;
+    }
+    .footban-text {
+        background-color:#C92828;
+        background-image: url(./image/footbanner.png);
+   background-repeat: no-repeat;
+   background-size: cover;
+    }
+.copyright{
+    background-color:#C92828;
+    text-align:center; 
+    color: #FFFFFF;
+    padding: 1rem 0 0.3rem 0;
+}
+    .footerrow{
+    
+    text-align: center;
+    margin: auto;
+    width: 50%;
+} 
+.footerrow2{
+   
+    padding: 3rem 0 3rem 0;
+    text-align: center;
+    margin: auto;
+    width: 50%;
+} 
+.footerrow2 img{
+    width:60px !important;
+}
+            .icon-link{
+                color:#FFFFFF;
+                text-decoration: none;
+            }
+            .icon-link>.bi {
+      display: inline-flex !important;
+      align-items: center !important;
+      text-decoration: none !important;
+      color: inherit v; /* Keeps the link color consistent with your theme */
+      gap: 8px !important; /* Adjust spacing between the icon and text */
+    }
+    .content{
+        background-color: #2C3E50;
+        border: 1px solid #FFFFFF;
+        border-radius: 12px;
+        padding:30px;
+        height:400px;
+        
+    }
+    a{
+        margin-top: 13px;
+    }
+    .link{
+      
+        color:#FFFFFF;
+        font-family:medium;
+    }
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main></main>
+        <footer>
+           <div class="footerbanner">
+            <div class="container">
+            <div class="row g-4 text-left">
+  <div class="col-sm-6 col-md-8">
+    <div class="content content1">
+      <h3 class="h3heading">Don't leave just yet, explore these resources!</h3>
+      <p>#BestDigitalMarketingInChennai #TopSocialMediaAgency #SEOExpertInGuindy #PPCManagementChennai #ContentMarketingSpecialist #GoogleAdsChennai #DigitalMarketingForSmallBusiness #TopSocialMediaMaintainer #DigitalMarketingForSmallBusinesses #AffordableDigitalMarketingChennai #GoogleMyBusinessOptimization 
+        #SocialMediaAdsManagement #BestPPCServicesGuindy #EmailMarketingChennai #SEOConsultantsInChennai #TopMarketingAgencyChennai </p>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+  <div class="content">
+    <h4 class="h4heading">Get in Touch</h4>
+    <p>Have a questions?<span class="bolditalic">Contact us</span> </p>
+    <a class="icon-link" href="mailto:example@example.com">
+    <i class="bi bi-telephone-fill"></i>
+    +91 73058 21333
+</a><br>
+    <a class="icon-link" href="mailto:example@example.com">
+    <i class="bi bi-envelope-fill"></i>
+    rainbowmedia@gmail.com
+</a><br>
+<a class="icon-link" href="mailto:example@example.com">
+    <i class="bi bi-geo-alt-fill"></i>
+    Old.No. 83, New no.112,<br>
+2nd floor, Anna salai, Manickam lane<br>
+Guindy, Chennai-600032<br>
+Landmark: Before street of Klase hotel, Guindy<br>
+
+</a>
+</div>
+  </div>
+</div>
+            </div>
+            
+
+           </div>
+           <div class="footban" >
+         
+            <!-- <img src="./image/footbanner.png" alt="footbanner" width="100%";> -->
+            <h3 class="h3heading">Have Any Project in Mind ?</h3>
+            <div class="text1">
+       <div class="container">
+  <div class="row footerrow">
+    <div class="col">
+     
+      <a class="link" href="">About Us</a>
+  
+    </div>
+    <div class="col">
+   
+    <a class="link" href="">Services</a>
+ 
+    </div>
+    <div class="col">
+    <a class="link" href="">Contact Us</a>
+    </div>
+    <div class="col">
+    <a class="link" href="">Careers</a>
+    </div>
+  </div>
+  <div class="row footerrow2" >
+    <div class="col">
+     
+      <a class="link" href="">
+      <img src="./image/insta.png" alt="instaimage">
+      </a>
+  
+    </div>
+    <div class="col">
+   
+    <a class="link" href="">
+    <img src="./image/fcb.png" alt="fcbimage">
+      </a>
+ 
+    </div>
+    <div class="col">
+    <a class="link" href="">
+      <img src="./image/Ln.png" alt="lnimage">
+      </a>
+    </div>
+    <div class="col">
+    <a class="link" href="">
+      <img src="./image/twit.png" alt="twitterimage">
+      </a>
+    </div>
+    <div class="col">
+    <a class="link" href="">
+      <img src="./image/yout.png" alt="youtubeimage">
+      </a>
+    </div>
+  </div>
+  </div>    
+            </div>
+           </div>
+           <div class="copyright">
+            <p>&copy; Copyright © 2024 Rainbow media. All rights reserved</p>
+           </div>
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
